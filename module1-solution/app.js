65e7200a2780dffcfe58ec681546de12caa98e2a
Oleksandr Kostur <ko.shandor@protonmail.com>
2018-03-02 23:51:32 +0200

    first commit

---
+++ module1-solution/app.js
@@ -0,0 +1,36 @@
+(function() {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+
+function LunchCheckController($scope) {
+    $scope.result = '';
+    $scope.color = 'green';
+
+    $scope.submitHandler = function() {
+        var parsedString = $scope.itemsString && parseString($scope.itemsString);
+
+        switch (true) {
+            case !parsedString:
+                $scope.result = "Please enter data first";
+                $scope.color = 'red';
+                break;
+            case parsedString.length > 3:
+                $scope.result = "Too much!";
+                $scope.color = 'green';
+                break;
+            default:
+                $scope.result = "Enjoy!";
+                $scope.color = 'green';
+        }
+    }
+}
+
+function parseString(string) {
+    return string.split(',').filter(Boolean);
+}
+
+})();
